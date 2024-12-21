17749da61547b7719048d233851cbd6730fc1c62
hmzsumon <zsumonn@gmail.com>
2024-12-21 21:13:25 +0600

    first commit

--- config/ioBaseUrl.ts
+++ config/ioBaseUrl.ts
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
 	ioBaseUrl = 'http://localhost:8000';
 	console.log('Running in development mode');
 } else {
-	ioBaseUrl = 'https://click-by-cash-71864725afcd.herokuapp.com';
+	ioBaseUrl = 'https://pyc-trade-api-329967f9dc4f.herokuapp.com';
 	// Code specific to production mode
 	console.log('Running in production mode');
 }
